13d4fe7dc96be3585bfa0f7adefe44b6f32dec5c
D134my <IceIsHere00@gmail.com>
2022-08-27 02:53:23 +0430

    update

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 1; 
 static const unsigned int gappih    = 13;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 13;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 13;       /* horiz outer gap between windows and screen edge */
@@ -10,7 +11,7 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=15" };
+static const char *fonts[]          = { "monospace:size=16","FontAwesome:size:13" };
 //static const char *fonts[]          = { "FontAwesome:pixelsize=18:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=16";
 static const char col_gray1[]       = "#222222";
@@ -49,10 +50,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ NULL,  NULL,       "Android Emulator - Pixel_5_API_30:5554",       1 << 8,       1,           -1 },
+	       /* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+       { "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+       { "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+       { "allacrity",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+       { NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+
+	{ "Alacritty",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,		  "spterm",		NULL,		SPTAG(0),		1,			 -1 },
 	{ NULL,		  "spfm",		NULL,		SPTAG(1),		1,			 -1 },
 	{ NULL,		  "keepassxc",	NULL,		SPTAG(2),		0,			 -1 },
